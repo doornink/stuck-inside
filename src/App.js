91519c4427da45654dfeb29a128686db657c3b64
Chris Doornink <chrisdoornink@simple.com>
2020-04-18 15:32:25 -0700

    so many things. Starting timer work

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
+import Profile from './pages/profile';
+import Lobby from './pages/lobby';
+import Signup from './pages/signup';
+import Login from './pages/login';
+import Game from './pages/game';
+import { auth } from './services/firebase';
+import PrivateRoute from './helpers/private-route';
+import PublicRoute from './helpers/public-route';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      authenticated: false,
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({
+          authenticated: true,
+          loading: false,
+        });
+      } else {
+        this.setState({
+          authenticated: false,
+          loading: false,
+        });
+      }
+    });
+  }
+
+  render() {
+    return this.state.loading === true ? (
+      <h2>Loading...</h2>
+    ) : (
+      <Router>
+        <Switch>
+          <Redirect exact path="/" to="login"></Redirect>
+          <PublicRoute
+            path="/login"
+            authenticated={this.state.authenticated}
+            component={Login}
+          />
+          <PublicRoute
+            path="/signup"
+            authenticated={this.state.authenticated}
+            component={Signup}
+          />
+
+          <PrivateRoute
+            path="/profile"
+            authenticated={this.state.authenticated}
+            component={Profile}
+          />
+
+          <PrivateRoute
+            path="/lobby"
+            authenticated={this.state.authenticated}
+            component={Lobby}
+          ></PrivateRoute>
+
+          <PrivateRoute
+            path="/game/:id"
+            authenticated={this.state.authenticated}
+            component={Game}
+          ></PrivateRoute>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
