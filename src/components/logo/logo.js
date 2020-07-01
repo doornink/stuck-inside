8d812af576a5d8460f681ef1812da73a224df090
Chris Doornink <chrisdoornink@simple.com>
2020-07-01 15:37:48 -0700

    metadata yo

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -3,5 +3,5 @@ import React from 'react';
 import './logo.css';
 
 export default function Logo({ title }) {
-  return <div className="logo">{title || 'VideoChat Games!'}</div>;
+  return <div className="logo">{title || 'Stuck Inside Games'}</div>;
 }
